e507e7157dc133689636f8e67eb379bb8ef6253e
tedh-shopbot <ted@shopbottools.com>
2016-12-04 21:25:43 -0500

    TEMP REMOVE m30 sent from G2.js

--- g2.js
+++ g2.js
@@ -730,8 +730,8 @@ G2.prototype.command = function(obj) {
 // Send a (possibly multi-line) string
 // An M30 will be placed at the end to put the machine back in the "idle" state
 G2.prototype.runString = function(data, callback) {
-log.debug("at g2runString > " + callback);
-	this.runSegment(data + "\nM30\n", callback);
+//	this.runSegment(data + "\nM30\n", callback);
+	this.runSegment(data, callback);
 };
 
 G2.prototype.runImmediate = function(data, callback) {
