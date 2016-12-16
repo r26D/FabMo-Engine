5a40372af736b6e0d103de7493551b66e9c061a3
tedh-shopbot <ted@shopbottools.com>
2016-12-16 08:33:02 -0500

    updating api

--- dashboard/static/js/libs/fabmoapi.js
+++ dashboard/static/js/libs/fabmoapi.js
@@ -383,7 +383,8 @@ FabMoAPI.prototype.manualMoveFixed = function(axis, speed, distance) {
 
 //TH for livecode from manual template; ...updated with "callback" fix to match fabmo.js
 FabMoAPI.prototype.livecodeStart = function(axis, speed, callback) {
-	this.executeRuntimeCode('livecode', {'cmd': 'start', 'axis' : axis, 'speed' : speed}, callback);
+//	this.executeRuntimeCode('livecode', {'cmd': 'start', 'axis' : axis, 'speed' : speed}, callback);
+	this.executeRuntimeCode('livecode', {'cmd': 'start', 'locX' : locX, 'locY' : locY}, callback);
 }
 
 FabMoAPI.prototype.livecodeHeartbeat = function(callback) {
