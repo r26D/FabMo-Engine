e15bfc8a98990176f7d691900086e04d8bf0afbb
tedh-shopbot <ted@shopbottools.com>
2017-01-13 14:34:53 -0500

    expt with initial block

--- g2.js
+++ g2.js
@@ -33,7 +33,7 @@ var EXPECT_TIMEOUT = 300000;
 // the machine has more time to run away before stopping.
 var JOG_TIMEOUT = 500;
 
-var GCODE_BLOCK_SEND_SIZE = 4;
+var GCODE_BLOCK_SEND_SIZE = 40;
 var GCODE_MIN_LINE_THRESH = 250;
 
 // Map used by the jog command to turn incoming direction specifiers to g-code
